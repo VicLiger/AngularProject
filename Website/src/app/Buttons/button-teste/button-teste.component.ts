1ac77f1eb2c9316c1d2a858dbef5bf9074d51f2c
Victor <victor.liger0800@gmail.com>
2024-07-12 23:04:41 -0300

    Aprendendo a inserir conteudos no componente

---
+++ Website/src/app/Buttons/button-teste/button-teste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-teste',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-teste.component.html',
+  styleUrl: './button-teste.component.css'
+})
+export class ButtonTesteComponent {
+      ButtonName:string = "Aprendendo a passar conteudo"
+}
